24606ca9b7566c5c1a183d1eacbb9deb898b176a
pepvalencia <pepvalencia@users.noreply.github.com>
2021-12-10 14:38:06 +0100

    products page with bootstrap table

---
+++ src/main/resources/script_insertProducts.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM db_naturally.product
+LIMIT 0, 1000
+
+-- Date: 2021-12-10 12:20
+*/
+INSERT INTO `` (`id`,`description`,`image_url`,`price`,`product_code`,`product_name`,`release_date`,`star_rating`) VALUES (1,'Leaf rake with 48-inch wooden handle','assets/images/leaf_rake.png',19.9,'GDN-0011','Leaf Rake','2021-12-09 09:40:46.000000',3.2);
+INSERT INTO `` (`id`,`description`,`image_url`,`price`,`product_code`,`product_name`,`release_date`,`star_rating`) VALUES (2,'15 gallon capacity rolling garden cart','assets/images/garden_cart.png',32.99,'GDN-0023','Garden Cart','2021-12-10 12:18:45.500000',4.2);
+INSERT INTO `` (`id`,`description`,`image_url`,`price`,`product_code`,`product_name`,`release_date`,`star_rating`) VALUES (3,'Curved claw steel hammer','assets/images/hammer.png',8.9,'TBX-0048','Hammer','2021-12-10 12:19:02.600000',4.8);
+INSERT INTO `` (`id`,`description`,`image_url`,`price`,`product_code`,`product_name`,`release_date`,`star_rating`) VALUES (4,'15-inch steel blade hand saw','assets/images/saw.png',11.55,'TBX-0022','Saw','2021-12-10 12:19:14.000000',3.7);
